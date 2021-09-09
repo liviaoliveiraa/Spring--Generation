ac1fc020b6d517a10d34d16e2d7460ccfa1a6b68
Lívia Oliveira <livia.oliveira.almeida@hotmail.com>
2021-09-09 11:22:27 -0300

    add security

---
+++ loja-de-games/src/main/java/br/org/generation/lojadegames/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package br.org.generation.lojadegames.repository;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import br.org.generation.lojadegames.model.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+	public Optional<Usuario> findByUsuario(String Usuario);
+
+}
